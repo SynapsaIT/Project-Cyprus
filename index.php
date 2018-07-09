89ff645d38c1376915a68c7fdf7f2a05131ad3bd
rado97 <rado97pl@gmail.com>
2018-07-09 12:29:18 +0200

    v0.01

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  $link = md5(time());
+  header("Location: test.php?l=".$link); /* Redirect browser */
+  /* Make sure that code below does not get executed when we redirect. */
+  exit;
+
+
+
+
+ ?>
---
+++ test.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+    </body>
+  </html>
+
+<?php
+
+$l = $_GET['l'];
+if(!isset($_POST['plik'])){
+  echo '<html>
+   <body>
+    <form action=pass.php'.'?l='.$l.' method="POST" ENCTYPE="multipart/form-data">
+
+     <div class="file-field input-field">
+        <div class="btn">
+          <span>File</span>
+          <input type="file" name="file[]" multiple>
+        </div>
+        <div class="file-path-wrapper">
+          <input class="file-path validate" type="text" placeholder="Upload one or more files">
+        </div>
+      </div>
+
+     <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+     <i class="material-icons right">send</i>
+     </button>
+
+    </form>
+   </body>
+  </html>';
+}
+
+
+
+
+?>
