bc2324fd087b2c7a23b14a50a5205bd1f84e92c9
rado97 <rado97pl@gmail.com>
2018-07-11 11:16:29 +0200

    v0.06

--- test.php
+++ test.php
@@ -6,6 +6,7 @@
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
 
+
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
@@ -20,11 +21,26 @@
 $l = $_GET['l'];
 if(!isset($_POST['file'])){
   echo '
-    <form action=pass.php'.'?l='.$l.' method="POST" ENCTYPE="multipart/form-data">
+
+    <form class="center-align" action=pass.php'.'?l='.$l.' method="POST" ENCTYPE="multipart/form-data" style="margin:0 auto; width:50vh;">
+    <div class="hide-on-large-only">
+      <!-- Modal Structure -->
+      <div id="modal1" class="modal modal-fixed-footer">
+        <div class="modal-content">
+          <h4>Attention!</h4>
+          <p>You are using a mobile device. If you want to send more than one photo you need to take pictures using your internal camera app.</p>
+        </div>
+        <div class="modal-footer">
+          <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+        </div>
+      </div>
+    </div>
+
+
 
      <div class="file-field input-field">
         <div class="btn">
-          <span>File</span>
+          <span>Browse</span>
           <input type="file" name="file[]" multiple accept="image/*">
         </div>
         <div class="file-path-wrapper">
@@ -44,7 +60,17 @@ if(!isset($_POST['file'])){
 
 ?>
 </div>
+  <script type="text/javascript" src="jquery/jquery-3.3.1.min.js"></script>
   <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+  <script type="text/javascript">
+
+    $(document).ready(function(){
+      $('.modal').modal();
+      $('#modal1').modal('open');
+    });
+
+
+  </script>
 
   </body>
 </html>
