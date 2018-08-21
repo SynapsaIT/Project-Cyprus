94dcc0b5c514d87b4cdb9eb1a1e9918f196c4233
Kotleet <kotlarczyk.mateusz@gmail.com>
2018-08-21 10:25:53 +0200

    style

--- login.php
+++ login.php
@@ -9,6 +9,7 @@
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
     <title>Test</title>
   </head>
   <body>
